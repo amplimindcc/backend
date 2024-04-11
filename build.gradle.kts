92c0c5a8cf0836cb1aa4daf77418be5d692744ff
Stefan074000 <lindner634@gmail.com>
2024-04-11 21:33:17 +0200

    Added first examples for tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     api("io.jsonwebtoken:jjwt-api:0.12.5")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-mail")
@@ -34,9 +35,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-docker-compose:3.2.4")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.0-RC1")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("org.postgresql:postgresql:42.6.0")
 
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.springdoc:springdoc-openapi-kotlin:1.8.0")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
@@ -44,6 +47,9 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/de/amplimind/codingchallenge/controller/AdminControllerTest.kt
@@ -0,0 +1,88 @@
+package de.amplimind.codingchallenge.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import de.amplimind.codingchallenge.dto.request.ChangeUserRoleRequestDTO
+import de.amplimind.codingchallenge.dto.request.CreateProjectRequestDTO
+import de.amplimind.codingchallenge.model.UserRole
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.put
+
+/**
+ * Test class for [AdminController].
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class AdminControllerTest
+    @Autowired
+    constructor(
+        val mockMvc: MockMvc,
+        val objectMapper: ObjectMapper,
+    ) {
+        /**
+         * Test that a project can be added successfully.
+         */
+        @Test
+        @WithMockUser(username = "admin", roles = ["ADMIN"])
+        fun test_successful_project_add() {
+            val request =
+                CreateProjectRequestDTO(
+                    title = "Test Project",
+                    description = "This is a test description",
+                    active = true,
+                )
+
+            this.mockMvc.post("/v1/admin/project/add") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(request)
+            }.andExpect {
+                status { isOk() }
+            }
+        }
+
+        /**
+         * Makes sure a 200 OK will be returned when a role change succeeds
+         */
+        @Test
+        @WithMockUser(username = "admin", roles = ["ADMIN"])
+        fun test_successful_role_change() {
+            val successfulRequest =
+                ChangeUserRoleRequestDTO(
+                    email = "user@web.de",
+                    newRole = UserRole.ADMIN,
+                )
+
+            this.mockMvc.put("/v1/admin/change/role") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(successfulRequest)
+            }.andExpect {
+                status { isOk() }
+            }
+        }
+
+        /**
+         * Makes sure a 404 not found will be returned when trying to change the role of a user that does not exist.
+         */
+        @Test
+        @WithMockUser(username = "admin", roles = ["ADMIN"])
+        fun test_failure_role_change() {
+            val failureRequest =
+                ChangeUserRoleRequestDTO(
+                    email = "unknown@web.de",
+                    newRole = UserRole.ADMIN,
+                )
+
+            this.mockMvc.put("/v1/admin/change/role") {
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(failureRequest)
+            }.andExpect {
+                status { isNotFound() }
+            }
+        }
+    }
