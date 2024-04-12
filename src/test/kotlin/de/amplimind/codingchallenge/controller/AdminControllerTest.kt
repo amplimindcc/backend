f10bd92e98439632767315eb049145dc68968520
Soeren <soern03@gmail.com>
2024-04-12 20:58:46 +0200

    added testing db to tests

--- src/test/kotlin/de/amplimind/codingchallenge/controller/AdminControllerTest.kt
+++ src/test/kotlin/de/amplimind/codingchallenge/controller/AdminControllerTest.kt
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
 import org.springframework.test.web.servlet.put
@@ -19,6 +20,7 @@ import org.springframework.test.web.servlet.put
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 internal class AdminControllerTest
     @Autowired
     constructor(
