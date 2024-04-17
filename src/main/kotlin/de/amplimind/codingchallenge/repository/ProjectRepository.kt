7d564af929df7eb95c33f5e93d42dd21241b37fc
Vincent Flemmig <62429100+VincentFlemmig@users.noreply.github.com>
2024-04-17 12:11:25 +0200

    Update ProjectRepository.kt
    Methode hinzugefügt um alle Project Ids zu bekommen, die aktive sind

--- src/main/kotlin/de/amplimind/codingchallenge/repository/ProjectRepository.kt
+++ src/main/kotlin/de/amplimind/codingchallenge/repository/ProjectRepository.kt
@@ -3,4 +3,11 @@ package de.amplimind.codingchallenge.repository
 import de.amplimind.codingchallenge.model.Project
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProjectRepository : JpaRepository<Project, Long>
+interface ProjectRepository : JpaRepository<Project, Long>{
+    /**
+     * returns all ids from Projects which are active
+     */
+
+    @Query("SELECT p.id FROM projects p where r.active = true")
+    fun findAllActiveProjects(): List<long>?
+}
