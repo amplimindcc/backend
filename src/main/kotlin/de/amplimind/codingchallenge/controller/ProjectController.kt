9341becf392834681cfff29745a9fbe4d1a292ca
Soeren <soern03@gmail.com>
2024-04-17 21:28:40 +0200

    Added email validation to project check

--- src/main/kotlin/de/amplimind/codingchallenge/controller/ProjectController.kt
+++ src/main/kotlin/de/amplimind/codingchallenge/controller/ProjectController.kt
@@ -3,6 +3,7 @@ package de.amplimind.codingchallenge.controller
 import de.amplimind.codingchallenge.dto.UserProjectDTO
 import de.amplimind.codingchallenge.service.ProjectService
 import de.amplimind.codingchallenge.service.SubmissionService
+import de.amplimind.codingchallenge.utils.ValidationUtils
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.http.ResponseEntity
@@ -31,10 +32,12 @@ class ProjectController(
     @Operation(summary = "Get project of user")
     @ApiResponse(responseCode = "200" , description = "Project fetched successfully")
     @ApiResponse(responseCode = "404", description = "User does not have a project assigned")
+    @ApiResponse(responseCode = "422", description = "Supplied email is not an email")
     @GetMapping("fetch/{email}")
     fun getUserProject(
         @PathVariable email : String,
     ): ResponseEntity<UserProjectDTO> {
+        ValidationUtils.validateEmail(email)
         return ResponseEntity.ok(this.projectService.fetchProjectById(
             this.submissionService.getProjectIdOfUser(email)
         ))
