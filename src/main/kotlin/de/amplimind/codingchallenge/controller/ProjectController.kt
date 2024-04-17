92358661de9f320fe25dd2daed1edd7581e987f5
Soeren <soern03@gmail.com>
2024-04-17 21:49:55 +0200

    refactored fetch project endpoints

--- src/main/kotlin/de/amplimind/codingchallenge/controller/ProjectController.kt
+++ src/main/kotlin/de/amplimind/codingchallenge/controller/ProjectController.kt
@@ -3,10 +3,12 @@ package de.amplimind.codingchallenge.controller
 import de.amplimind.codingchallenge.dto.UserProjectDTO
 import de.amplimind.codingchallenge.service.ProjectService
 import de.amplimind.codingchallenge.service.SubmissionService
+import de.amplimind.codingchallenge.utils.UserUtils
 import de.amplimind.codingchallenge.utils.ValidationUtils
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -33,11 +35,9 @@ class ProjectController(
     @ApiResponse(responseCode = "200" , description = "Project fetched successfully")
     @ApiResponse(responseCode = "404", description = "User does not have a project assigned")
     @ApiResponse(responseCode = "422", description = "Supplied email is not an email")
-    @GetMapping("fetch/{email}")
-    fun getUserProject(
-        @PathVariable email : String,
-    ): ResponseEntity<UserProjectDTO> {
-        ValidationUtils.validateEmail(email)
+    @GetMapping("/fetch")
+    fun getUserProject(): ResponseEntity<UserProjectDTO> {
+        val email: String = UserUtils.fetchLoggedInUser().username
         return ResponseEntity.ok(this.projectService.fetchProjectById(
             this.submissionService.getProjectIdOfUser(email)
         ))
