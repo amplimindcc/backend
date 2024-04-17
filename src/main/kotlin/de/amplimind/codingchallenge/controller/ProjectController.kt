96ddc3806d41d46a95ab8065b9d0efb0bf7335fa
Soeren <soern03@gmail.com>
2024-04-17 21:23:28 +0200

    Added Endpoint to check which project user is assigned

--- src/main/kotlin/de/amplimind/codingchallenge/controller/ProjectController.kt
+++ src/main/kotlin/de/amplimind/codingchallenge/controller/ProjectController.kt
@@ -1,9 +1,13 @@
 package de.amplimind.codingchallenge.controller
 
+import de.amplimind.codingchallenge.dto.UserProjectDTO
 import de.amplimind.codingchallenge.service.ProjectService
+import de.amplimind.codingchallenge.service.SubmissionService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/v1/project")
 class ProjectController(
+    private val submissionService: SubmissionService,
     private val projectService: ProjectService,
 ) {
     @Operation(summary = "Endpoint for deleting a project.")
@@ -21,5 +26,17 @@ class ProjectController(
         @PathVariable projectId: Long,
     ) {
         projectService.deleteProject(projectId)
+    }
+
+    @Operation(summary = "Get project of user")
+    @ApiResponse(responseCode = "200" , description = "Project fetched successfully")
+    @ApiResponse(responseCode = "404", description = "User does not have a project assigned")
+    @GetMapping("fetch/{email}")
+    fun getUserProject(
+        @PathVariable email : String,
+    ): ResponseEntity<UserProjectDTO> {
+        return ResponseEntity.ok(this.projectService.fetchProjectById(
+            this.submissionService.getProjectIdOfUser(email)
+        ))
     }
 }
---
+++ src/main/kotlin/de/amplimind/codingchallenge/dto/UserProjectDTO.kt
@@ -0,0 +1,9 @@
+package de.amplimind.codingchallenge.dto
+/**
+ * Data transfer object for description of user project
+ * * @param description description of project
+ */
+class UserProjectDTO(
+    val title: String,
+    val description: String
+)
