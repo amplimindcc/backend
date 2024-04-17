c82a95384cd2860926e5330212aefe0acc7d7c57
str9990 <str9990@thi.de>
2024-04-17 16:16:34 +0200

    Added deleteProject Endpoint

--- src/main/kotlin/de/amplimind/codingchallenge/controller/ProjectController.kt
+++ src/main/kotlin/de/amplimind/codingchallenge/controller/ProjectController.kt
@@ -2,7 +2,21 @@ package de.amplimind.codingchallenge.controller
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
 @RequestMapping("/v1/project")
-class ProjectController
+class ProjectController(
+    private val projectService: ProjectService,
+) {
+    @Operation(summary = "Endpoint for deleting a project.")
+    @ApiResponse(responseCode = "200", description = "Project was deleted successfully")
+    @DeleteMapping("/project/{projectId}")
+    fun deleteProject (
+        @PathVariable projectId: Long
+    ) {
+        projectService.deleteProject(projectId)
+    }
+}
