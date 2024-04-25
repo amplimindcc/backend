4ba2637a0a6b5de79c60b7a19833d732d4ca6f3c
Soeren <soern03@gmail.com>
2024-04-25 17:57:26 +0200

    added endpoint to return whether a token is valid or not

--- src/main/kotlin/de/amplimind/codingchallenge/controller/AuthController.kt
+++ src/main/kotlin/de/amplimind/codingchallenge/controller/AuthController.kt
@@ -3,6 +3,7 @@ package de.amplimind.codingchallenge.controller
 import de.amplimind.codingchallenge.dto.request.LoginRequestDTO
 import de.amplimind.codingchallenge.dto.request.RegisterRequestDTO
 import de.amplimind.codingchallenge.service.UserService
+import de.amplimind.codingchallenge.utils.JWTUtils
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import jakarta.servlet.http.HttpSession
@@ -14,6 +15,7 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -71,5 +73,17 @@ class AuthController(
         }
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User is not authenticated")
+    }
+
+    @Operation(summary = "Checks whether a token is valid or not")
+    @ApiResponse(responseCode = "200", description = "Token is valid")
+    @ApiResponse(responseCode = "400", description = "Token is invalid")
+    @ApiResponse(responseCode = "403", description = "Token is expired")
+    @GetMapping("/check-token/{token}")
+    fun checkTokenValidity(
+        @PathVariable token: String,
+    ): ResponseEntity<String>  {
+        JWTUtils.validateToken(token)
+        return ResponseEntity.ok("Token is valid")
     }
 }
