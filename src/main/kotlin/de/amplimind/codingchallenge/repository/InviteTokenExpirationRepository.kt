6627189f665a606f307f40b606e4e127ad6c0338
Stefan074000 <lindner634@gmail.com>
2024-04-25 20:39:59 +0200

    Added endpoint to check for expiration date of its invite

--- src/main/kotlin/de/amplimind/codingchallenge/repository/InviteTokenExpirationRepository.kt
+++ src/main/kotlin/de/amplimind/codingchallenge/repository/InviteTokenExpirationRepository.kt
@@ -7,7 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository
  * Repository for invite tokens.
  */
 interface InviteTokenExpirationRepository : JpaRepository<InviteTokenExpiration, Long> {
+
+    /**
+     * Deletes an entry for a user.
+     * @param email the email of the user
+     */
     fun deleteByEmail(email: String)
 
+    /**
+     * Fetches an entry for a user.
+     * @param email the email of the user
+     * @return the entry or null if non existing
+     */
     fun findByEmail(email: String): InviteTokenExpiration?
 }
