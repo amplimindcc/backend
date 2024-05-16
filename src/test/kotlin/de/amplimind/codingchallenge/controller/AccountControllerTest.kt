3c54e3deb37edec26e87593be115547cb15e7c1f
Soeren <soern03@gmail.com>
2024-05-16 15:31:15 +0200

    formatting code

--- src/test/kotlin/de/amplimind/codingchallenge/controller/AccountControllerTest.kt
+++ src/test/kotlin/de/amplimind/codingchallenge/controller/AccountControllerTest.kt
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
@@ -62,7 +61,9 @@ internal class AccountControllerTest
         fun setUp() {
             MockKAnnotations.init(this)
             TestDataInitializer(
-                userRepository,submissionRepository,projectRepository
+                userRepository,
+                submissionRepository,
+                projectRepository,
             ).initTestData()
         }
 
