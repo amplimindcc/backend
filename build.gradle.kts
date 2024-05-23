416c54bea133fa63fe8ba5d7a14809bf25cc2e26
Stefan074000 <lindner634@gmail.com>
2024-05-23 11:59:24 +0200

    Unified versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,13 @@ java {
     sourceCompatibility = JavaVersion.VERSION_21
 }
 
+val commonSpringBootVersion: String by extra
+val coroutinesVersion: String by extra
+val retrofit2Version: String by extra
+val mockKVersion: String by extra
+val jjwtVersion: String by extra
+val postgresVersion: String by extra
+
 repositories {
     mavenCentral()
 }
@@ -29,49 +36,38 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-mail")
-    implementation("org.springframework.session:spring-session-core:3.2.2")
-    implementation("org.springframework.session:spring-session-data-redis:3.2.2")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis:3.2.4")
-    implementation("org.springframework.data:spring-data-redis:3.2.4")
-    implementation("org.springframework.boot:spring-boot-docker-compose:3.2.4")
+    implementation("org.springframework.session:spring-session-core:$commonSpringBootVersion")
+    implementation("org.springframework.session:spring-session-data-redis:$commonSpringBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:$commonSpringBootVersion")
+    implementation("org.springframework.data:spring-data-redis:$commonSpringBootVersion")
+    implementation("org.springframework.boot:spring-boot-docker-compose:$commonSpringBootVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.0-RC1")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
-    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("org.postgresql:postgresql:$postgresVersion")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:retrofit-mock:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$coroutinesVersion")
+    implementation("com.squareup.retrofit2:retrofit:$retrofit2Version")
+    implementation("com.squareup.retrofit2:retrofit-mock:$retrofit2Version")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:$retrofit2Version")
     implementation("com.squareup.retrofit2:converter-gson:2.3.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     implementation("commons-validator:commons-validator:1.8.0")
 
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:retrofit-mock:2.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.3.0")
-
     runtimeOnly("org.springdoc:springdoc-openapi-kotlin:1.8.0")
     runtimeOnly("com.mysql:mysql-connector-j")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-orgjson:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-orgjson:$jjwtVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("io.mockk:mockk:$mockKVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
 
